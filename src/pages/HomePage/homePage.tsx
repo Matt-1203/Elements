d7ddf2136ad037e263702e6baeb9d515bbe0e978
Matt-1203 <matthewjtaylor03@outlook.com>
2026-08-10 13:01:36 +0100

    Update Homepage
    Add mobile CSS layouts for homepage (testing to be built to other pages as well)

--- src/pages/HomePage/homePage.tsx
+++ src/pages/HomePage/homePage.tsx
@@ -1,39 +1,33 @@
-import videoBg from '../../assets/VideoBackground.mp4';
+import videoBg from "../../assets/VideoBackground.mp4";
 import Logo from "../../assets/LogoElements.png";
 import DashboardHeader from "../../components/Header";
 import DashboardFooter from "../../components/Footer";
+import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div className="main" style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
-      <div className="overlay"
-        style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.4)", zIndex: 1,}}
-      />
+    <main className="main">
+      {/* Background */}
+      <video className="backgroundVideo" src={videoBg} autoPlay loop muted playsInline/>
+      <div className="overlay" />
 
-      {/*Background Video */}
-      <video src={videoBg} autoPlay loop muted playsInline
-        style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: 0,}}
-      />
-
-      {/*Header*/}
-      <div style={{ position: "absolute", top: 0, left: 0, width: "100%", zIndex: 10 }}>
+      {/* Header */}
+      <header className="header">
         <DashboardHeader />
-      </div>
+      </header>
 
-      {/*Centered Content */}
-      <div className="content"
-        style={{position: "relative", zIndex: 2, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", color: "#ffffff", textAlign: "center"}}
-      >
-        <img src={Logo} alt="Your Logo" style={{ width: "8%", height: "auto", padding: "50px" }} />
-        <h1 style={{ fontSize: "3rem", margin: 0 }}>PHOTOGRAPHY//ELEMENTS</h1>
-        <p style={{ fontSize: "1rem", fontWeight: "bold" }}>Matthew Taylor</p>
-      </div>
-      
-      {/*Footer*/}
-      <div style={{ position: "absolute", bottom: 0, left: 0, width: "100%", zIndex: 10 }}>
+      {/* Main content */}
+      <section className="content">
+        <img src={Logo} alt="Photography Elements"className="logo"/>
+        <h1>PHOTOGRAPHY//ELEMENTS</h1>
+        <p>Matthew Taylor</p>
+      </section>
+
+      {/* Footer */}
+      <footer className="footer">
         <DashboardFooter />
-      </div>
-    </div>
+      </footer>
+    </main>
   );
 };
 
