7e6bea8628f3f7a1c09b71e055bc842890b47049
Matt-1203 <matthewjtaylor03@outlook.com>
2026-09-15 13:01:19 +0100

    Remove Test Bench

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import HomePage from "./pages/HomePage/homePage";
-import Test from "./pages/Test/test"
 import ElementsPage from "./pages/ElementsPage/elementsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GalleryOne from "./pages/Gallery/galleryPage";
@@ -10,7 +9,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={<Test />} />
         <Route path="/elements" element={<ElementsPage />} />
         <Route path="/gallery" element={<Carousel />} />
         <Route path="/gallery/collection-one" element={<GalleryOne />} />
